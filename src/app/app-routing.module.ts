03cd743edac2afef5780b4ffbea861956c90f621
Harsh Tiwari <harshtiwari8765@gmail.com>
2019-01-17 12:08:29 +0530

    daily article  updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './Component/HomeComponent/home/home.component';
 import { GetEnquiryComponent } from './Component/enquirycomponent/get-enquiry/get-enquiry.component';
@@ -7,6 +7,7 @@ import { LogInComponent } from './component/log-in/log-in/log-in.component';
 import { RegistrationComponent } from './component/registration/registration/registration.component';
 import { ContactUsComponent } from './component/contact-Us/contact-us/contact-us.component';
 import { Home2Component } from './component/home/home2/home2.component';
+import {AboutUsComponent } from "./Component/about-us/about-us.component";
 const routes: Routes = [
   {path:"home",component:HomeComponent},
   {path:"home2",component:Home2Component},
@@ -15,6 +16,7 @@ const routes: Routes = [
   {path:"InsertEnquiry",component:InsertEnquiryComponent},
   {path:"Contact",component:ContactUsComponent},
   {path:"registration",component: RegistrationComponent},
+  {path:"Aboutus",component:AboutUsComponent},
   { path: '', redirectTo: '/home2', pathMatch:'full' }
   ];
 
