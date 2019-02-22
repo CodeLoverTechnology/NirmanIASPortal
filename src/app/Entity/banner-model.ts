73c27482955a8b85251ec966ab0a0042b0a1b9f4
Harsh Tiwari <harshtiwari8765@gmail.com>
2019-02-22 19:20:10 +0530

    all service are get and branch is inserting

--- src/app/Entity/banner-model.ts
+++ src/app/Entity/banner-model.ts
@@ -4,7 +4,7 @@ export class BannerModel {
         public BannerTitle    :string,
         public BannerImagesPath:   string,
         public SubTitle  :   string,
-        public Remarks:string,
+        public Remarks: string,
          public CreatedBy   :   string,
          public CreatedDate   : string,
          public ModifiedBy  :  string ,
